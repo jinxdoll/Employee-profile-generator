5f5e806e7990fbc7636def1161c27b7827a59f59
Aimee <amattson84@gmail.com>
2021-04-05 16:03:47 -0700

    required objects and dependencies.

---
+++ index.js
@@ -0,0 +1,54 @@
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
+
+const employee = new Employee();
+
+// function runInquireStart() {
+//     const promptArray = [
+//         {
+//         type: "input",
+//         message: "What is your name ?",
+//         name: "name",
+//     },
+//     {
+//         type: "input",
+//         message: "What is your id ?",
+//         name: "id",
+
+//     }, 
+//     {
+//         type: "input",
+//         message: "What is your email ?",
+//         name: "email",
+
+//     },
+//     {
+//         type: "list",
+//         message: "What is your role ?",
+//         choices: ["Manager", "Intern", "Engineer"],
+//         name: "role"
+//     }
+// ]
+//         return runInquireStart
+//         .prompt(promptArray);
+// }
+
+// // In addition to `Employee`'s properties and methods, `Manager` will also have the following:
+
+// function managerInquirer() {
+//     const promptArray = [{
+//         type: "input",
+//         message: " What is your Manager office number?",
+//         name: "officeNumber"
+//     }];
+
+//     return inquirer.prompt(promptArray);
+//     getRole()
+//     {
+//     return Manager
+//     }    
+// }
+
