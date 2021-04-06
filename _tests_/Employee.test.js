4e4266c45c3d35a53ca3d8dd792c04e6898614f8
Aimee <amattson84@gmail.com>
2021-04-05 17:43:36 -0700

    Refactored test for getId, getEmail, getRole methods.

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -18,9 +18,11 @@ const Employee = require("../lib/employee");
 describe("Employee Class", () => {
   describe("getName method", () => {
     it("creates a name for the employee class object", () => {
-      const employee = new Employee("New employee", [{ name: " " }]);
-      employee.getName("");
-      expect(employee.name).toBe("New employee");
+      const testName = "Aimee"
+      const employee = new Employee(testName);
+      // const employee = new Employee("New employee", [{ name: " " }]);
+      // employee.getName();
+      expect(employee.name).toBe(testName);
     });
   });
 });
@@ -28,9 +30,53 @@ describe("Employee Class", () => {
 describe("Employee Class", () => {
   describe("getId method", () => {
     it("creates a ID for the employee class object", () => {
-      const testValue = 101;
-      const id = new Employee("Employee ID", testValue);    
-      expect(id.getId()).toBe(testValue);
+      const testId = 101;
+      const id = new Employee("Aimee", testId);    
+      expect(id.getId()).toBe(testId);
     });
   });
 });
+
+
+describe("Employee Class", () => {
+  describe("getEmail method", () => {
+    it("creates a email for the employee class object", () => {
+      const testEmail = "newemployee@testEmail.com";
+      const email = new Employee("Aimee",101, testEmail);    
+      expect(email.getEmail()).toBe(testEmail);
+    });
+  });
+});
+
+describe("Employee Class", () => {
+  describe("getRole method", () => {
+    it("returns the role for the employee object", () => {
+      
+      const role = new Employee("Aimee",101, "newemployee@testEmail.com", "Employee");    
+      expect(role.getRole()).toBe("Employee");
+    });
+  });
+});
+
+
+
+
+// describe("Employee Class", () => {
+//   describe("getEmail method", () => {
+//     it("gets an email", () => {
+//       const email = new Email("New Email", [{ Email: "Employee Email" }]);
+//       employee.getEmail("Employee Email");
+//       expect(employee.Email).toBe("Employee Email");
+//     });
+//   });
+// });
+
+// describe("Employee Class", () => {
+//   describe("getRole method", () => {
+//     it("gets a Role", () => {
+//       const role = new Role("New Role", [{ role: "Employee Role" }]);
+//       employee.getRole("Employee Role");
+//       expect(employee.role).toBe(Employee);
+//     });
+//   });
+// });
