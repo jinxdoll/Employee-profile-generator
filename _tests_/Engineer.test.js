cebc8287e60c38ca63b95ca14fc8de55712723d1
Aimee <amattson84@gmail.com>
2021-04-05 20:37:16 -0700

    Deleted comments. Formatted with Prettier extension.

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,30 +1,40 @@
-const Engineer = require ("../lib/engineer");
-// const Employee = require("../lib/employee");
+const Engineer = require("../lib/engineer");
 
 describe("set Github account name", () => {
+  const testGit = "githubUser";
+  const engineer = new Engineer(
+    "Aimee",
+    101,
+    "newemployee@testEmail.com",
+    testGit
+  );
+  expect(engineer.github).toBe(testGit);
+});
+
+describe("Engineer Class", () => {
+  describe("getRole method", () => {
+    it("returns the role as 'Engineer' for the employee object", () => {
+      const testRole = "Engineer";
+      const role = new Engineer(
+        "Aimee",
+        101,
+        "newemployee@testEmail.com",
+        "githubUser"
+      );
+      expect(role.getRole()).toBe(testRole);
+    });
+  });
+});
+
+describe("getGithub method", () => {
+  it("gets the Github username for the Engineer class that extends the Employee object", () => {
     const testGit = "githubUser";
-        const engineer = new Engineer("Aimee", 101, "newemployee@testEmail.com", testGit); 
-    expect(engineer.github).toBe(testGit);
+    const engineer = new Engineer(
+      "Aimee",
+      101,
+      "newemployee@testEmail.com",
+      testGit
+    );
+    expect(engineer.getGithub()).toBe(testGit);
   });
-
-  describe("Engineer Class", () => {
-    describe("getRole method", () => {
-      it("returns the role as 'Engineer' for the employee object", () => {  
-          const testRole = "Engineer";    
-        const role = new Engineer("Aimee",101, "newemployee@testEmail.com", "githubUser");    
-        expect(role.getRole()).toBe(testRole);
-      });
-    });
-  });
-
-
-    describe("getGithub method", () => {
-      it("gets the Github username for the Engineer class that extends the Employee object", () => {
-        const testGit = "githubUser";
-        const engineer = new Engineer("Aimee", 101, "newemployee@testEmail.com", testGit); 
-        // const employee = new Employee("New employee", [{ name: " " }]);
-        // employee.getName();
-        expect(engineer.getGithub()).toBe(testGit);
-      });
-    });
- 
+});
--- index.js
+++ index.js
@@ -7,48 +7,44 @@ const Intern = require("./lib/intern");
 const employee = new Employee();
 
 function runInquireStart() {
-    const questions = [
-        {
-        type: "input",
-        message: "What is your name ?",
-        name: "name",
+  const questions = [
+    {
+      type: "input",
+      message: "What is your name ?",
+      name: "name",
     },
     {
-        type: "input",
-        message: "What is your id ?",
-        name: "id",
-
-    }, 
-    {
-        type: "input",
-        message: "What is your email ?",
-        name: "email",
-
+      type: "input",
+      message: "What is your id ?",
+      name: "id",
     },
     {
-        type: "list",
-        message: "What is your role ?",
-        choices: ["Manager", "Intern", "Engineer"],
-        name: "role"
-    }
-]
-        return runInquireStart
-        .prompt(questions);
+      type: "input",
+      message: "What is your email ?",
+      name: "email",
+    },
+    {
+      type: "list",
+      message: "What is your role ?",
+      choices: ["Manager", "Intern", "Engineer"],
+      name: "role",
+    },
+  ];
+  return runInquireStart.prompt(questions);
 }
-
-// // In addition to `Employee`'s properties and methods, `Manager` will also have the following:
 
 function managerInquirer() {
-    const promptArray = [{
-        type: "input",
-        message: " What is your Manager office number?",
-        name: "officeNumber"
-    }];
-
-    return inquirer.prompt(promptArray);
-    getRole()
+  const promptArray = [
     {
-    return Manager
-    }    
-}
+      type: "input",
+      message: " What is your Manager office number?",
+      name: "officeNumber",
+    },
+  ];
 
+  return inquirer.prompt(promptArray);
+  getRole();
+  {
+    return Manager;
+  }
+}
