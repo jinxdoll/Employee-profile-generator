fa46da8d28138e4cd1860d8a53ba36e3b024e491
Aimee <amattson84@gmail.com>
2021-04-07 13:31:10 -0700

    renderHtml function. html template for renderHtml function.

--- index.js
+++ index.js
@@ -258,3 +258,57 @@ function addEmployee() {
   console.log("creating your team (line 261)");
 }
 
+function renderHtml() {
+  let employeeCards = "";
+
+  employeeArray.forEach((item) => {
+    let cardText = item.createCard();
+    employeeCards += cardText;
+  });
+
+  let htmlTemplate = `
+           <!DOCTYPE html>
+        <html lang="en">
+           <head>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+              <link
+                 rel="stylesheet"
+                 href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                 integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                 crossorigin="anonymous"
+              />
+              <script
+                 src="https://kit.fontawesome.com/ab3fd93a87.js"
+                 crossorigin="anonymous"
+              ></script>
+              <title>My Team Roster</title>
+           </head>
+           <body>
+              <div
+                 class="container-fluid bg-danger text-center d-flex align-items-center justify-content-center"
+                 style="height: 20vh"
+              >
+                 <div class="h1 text-white" style="display: inline-block;">
+                    My Team
+                 </div>
+              </div>
+              <div class="container mt-5">
+                 <!-- start of card group -->
+                 <div class="card-deck d-inline-flex justify-content-center">
+                    ${employeeCards}
+                 </div>
+                 <!-- end of card group -->
+              </div>
+           </body>
+        </html>
+           `;
+  fs.writeFile("./html/teamPage.html", htmlTemplate, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+
+inquirerStart();
